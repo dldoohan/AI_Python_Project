bd4f4334e042de1a3ef26f8334ac0cbd0c190723
dldoohan <dldoohan@gmail.com>
2023-04-19 08:30:57 -0700

    Day 2: Data Types, Variables, and Basic Operations

---
+++ day2.py
@@ -0,0 +1,56 @@
+# Declare and initialize variables
+age = 25                  # Integer variable
+pi = 3.14159265359        # Float variable
+name = "John Doe"         # String variable
+is_student = True         # Boolean variable
+
+# Print the variables
+print("Age:", age)
+print("Pi value:", pi)
+print("Name:", name)
+print("Is student:", is_student)
+
+# Arithmetic operations
+addition = 7 + 3
+subtraction = 7 - 3
+multiplication = 7 * 3
+division = 7 / 3
+modulus = 7 % 3
+exponentiation = 7 ** 3
+floor_division = 7 // 3
+
+# Comparison operations
+equal = 7 == 3
+not_equal = 7 != 3
+greater_than = 7 > 3
+less_than = 7 < 3
+greater_than_or_equal_to = 7 >= 3
+less_than_or_equal_to = 7 <= 3
+
+# Logical operations
+result_and = True and False
+result_or = True or False
+result_not = not True
+
+# Print the results
+print("\nArithmetic Operations:")
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
+print("Modulus:", modulus)
+print("Exponentiation:", exponentiation)
+print("Floor Division:", floor_division)
+
+print("\nComparison Operations:")
+print("Equal:", equal)
+print("Not Equal:", not_equal)
+print("Greater Than:", greater_than)
+print("Less Than:", less_than)
+print("Greater Than or Equal To:", greater_than_or_equal_to)
+print("Less Than or Equal To:", less_than_or_equal_to)
+
+print("\nLogical Operations:")
+print("And:", result_and)
+print("Or:", result_or)
+print("Not:", result_not)
