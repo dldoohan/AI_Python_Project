41b29f4b2004e5f11dfcc1ab23975df61b8d7b5e
dldoohan <dldoohan@gmail.com>
2023-04-19 17:38:14 -0700

    Day 8: Working with Modules and Packages

---
+++ day8.py
@@ -0,0 +1,7 @@
+from utilities import math_operations
+
+result1 = math_operations.add(5, 3)
+result2 = math_operations.subtract(5, 3)
+
+print("Addition:", result1) # The result should be 8
+print("Subtraction:", result2) # The result should be 2
